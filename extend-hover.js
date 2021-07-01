68426cab6ffef1dcbbbb7226d718b6ea8a5f882d
Dbeezt <dbrereton1995@gmail.com>
2021-07-01 21:31:33 +0100

    Made loop prettier

--- extend-hover.js
+++ extend-hover.js
@@ -1,5 +1,5 @@
-const wavefinder_wheres = document.getElementsByClassName("wavefinder-where");
+let wavefinder_wheres = document.getElementsByClassName("wavefinder-where");
 
-for(let spot = 0; spot < wavefinder_wheres.length; spot++){
-    alert(wavefinder_wheres[spot].getElementsByTagName('a')[0].text)
+for(let spot of wavefinder_wheres){
+    // console.log(spot.innerText)
 }
